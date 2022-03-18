40fd3a8c7d600e9b56618f4253ccb53bf6123e8e
GabrielMüllerDS <61920654+GabrielMullerDS@users.noreply.github.com>
2022-03-17 22:24:59 -0300

    Update App.js

--- Atividades/01/App.js
+++ Atividades/01/App.js
@@ -7,8 +7,13 @@ export default function App() {
   let m;
 
   function converter() {
+    if (km <= 0){
+      Alert.alert('Impossível Converter ;-;', 'Por favor digite um número maior que 0 (zero)')
+    }
+    else{
     m = (km * 1000);
     Alert.alert('Convertido ^^', `A distância de ${km}km é igual a ${m}m`);
+    }
   }
   
   return (
